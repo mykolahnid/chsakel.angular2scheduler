28a4699cb8434e402f95e01a600bfed7b7d49deb
mykolahnid <g.mykola@gmail.com>
2016-09-08 18:10:12 +0300

    services, features

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+ 
+// Add the RxJS Observable operators we need in this app.
+import './rxjs-operators';
+ 
+@Component({
+    selector: 'scheduler',    
+})
+export class AppComponent {
+ 
+}
